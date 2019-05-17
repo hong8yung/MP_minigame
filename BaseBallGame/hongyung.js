7e7907258ea58bbeac32eb42ac8852067abaaa71
hong8yung <hoyun342@gmail.com>
2019-05-18 05:16:37 +0900

    apply shuffle function

--- BaseBallGame/hongyung.js
+++ BaseBallGame/hongyung.js
@@ -1,22 +1,36 @@
 (function(){
     function Controller($scope){
         $scope.gameStatus = "ready";
-        $scope.chalNum;
-        $scope.chalHistory = [];
+        $scope.botNum;
+        $scope.botHistory = [];
         $scope.result;
         $scope.resultHistory = [];
 
         var bot;
+        var botGuessNumbers;
+        var checkResult;
 
         $scope.resInput = function(){
             if($scope.gameStatus == 'ready'){
                 $scope.gameStatus = 'play';
-                bot = new Bot();
 
-                $scope.chalHistory = [];
+                bot = new Bot();
+                checkResult = new CheckResult(false, 0, 0);
+
+                $scope.result = "";
+                $scope.botHistory = [];
                 $scope.resultHistory = [];
+
+                botGuessNumbers = bot.getNextGuess();
+                $scope.botHistory.push({numbers:botGuessNumbers.toString(), checkResult:checkResult});
+                console.log("first : "+botGuessNumbers.toString());
             }else if($scope.gameStatus == 'play'){
-                
+                console.log($scope.result)
+                $scope.result = "";
+
+                botGuessNumbers = bot.getNextGuess();
+                $scope.botHistory.push({numbers:botGuessNumbers.toString(), checkResult:checkResult});
+                console.log(botGuessNumbers.toString());
             }else if($scope.gameStatus == 'over'){
                 $scope.gameStatus = 'ready';
             }
@@ -25,4 +39,20 @@
 
     var firstApp = angular.module("baseBallApp", []);
     firstApp.controller("Controller", Controller);
+    firstApp.directive('historyList', function(){
+        return {
+            restrict: 'A'
+            ,scope: {
+                gameHistory: '=gameHistory'
+            }
+            ,template: '<p>'
+								+ '<p class="rbtn pure-button">{{gameHistory.numbers[0]}}</p>'
+								+ '<p class="rbtn pure-button">{{gameHistory.numbers[1]}}</p>'
+								+ '<p class="rbtn pure-button">{{gameHistory.numbers[2]}}</p>'
+								+ ' -> ' 
+								+ '<p class="rbtn pure-button">{{gameHistory.checkResult.strikeCount}}S  {{gameHistory.checkResult.ballCount}}B</p>'
+								+ '</p>'
+						// ,templateUrl: 'history_template.html'
+        };
+    });
 })();
